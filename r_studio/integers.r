c19ebff10a5a1deda1bbd5c5847ee106d2bf8504
tphyusinhtut-web <tphyusinhtut@gmail.com>
2025-11-16 21:13:56 +0800

    Add files via upload

---
+++ r_studio/integers.r
@@ -0,0 +1,21 @@
+# Assignment 1 Part 2
+
+# print integers 1 to 10
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+print(6)
+print(7)
+print(8)
+print(9)
+print(10)
+
+# print integers 1 to 10 (another way)
+print(1:10)
+
+# print integers 1 to 10 (using a for loop)
+for(i in c(1:10)) {
+    print(i)
+}
